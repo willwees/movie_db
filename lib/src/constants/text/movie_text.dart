9d693cbe9d16925d010f96c07025e35af8b0105f
William Suyanto <william.suyanto@gmail.com>
2022-05-26 18:52:42 +0700

    add MovieText constant

---
+++ lib/src/constants/text/movie_text.dart
@@ -0,0 +1,6 @@
+class MovieText {
+  static const String movie = 'Movie';
+  static const String nowPlaying = 'Now Playing';
+  static const String upcoming = 'Upcoming';
+  static const String popular = 'Popular';
+}
