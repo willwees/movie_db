66d47e9c6a80f6810b03e98626c0b05715476e68
William Suyanto <william.suyanto@gmail.com>
2022-05-27 00:53:15 +0700

    bind tv show bloc to UI

--- lib/src/features/tv_show/ui/tv_show_screen.dart
+++ lib/src/features/tv_show/ui/tv_show_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moviedb/src/constants/text/tv_show_text.dart';
+import 'package:moviedb/src/features/movie/ui/widgets/movie_card_list_shimmer_widget.dart';
+import 'package:moviedb/src/features/tv_show/bloc/tv_show/tvshow_bloc.dart';
+import 'package:moviedb/src/features/tv_show/ui/widgets/tv_show_card_list_widget.dart';
 
 class TVShowScreen extends StatefulWidget {
   const TVShowScreen({Key? key}) : super(key: key);
@@ -9,6 +13,13 @@ class TVShowScreen extends StatefulWidget {
 }
 
 class _TVShowScreenState extends State<TVShowScreen> with AutomaticKeepAliveClientMixin {
+  @override
+  void initState() {
+    super.initState();
+    context.read<TVShowBloc>().add(TVShowGetTVOnTheAirListEvent());
+    context.read<TVShowBloc>().add(TVShowGetPopularListEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -34,11 +45,40 @@ class _TVShowScreenState extends State<TVShowScreen> with AutomaticKeepAliveClie
   }
 
   Widget _buildTVOnTheAirSection() {
-    return Container();
+    return BlocBuilder<TVShowBloc, TVShowState>(
+      bloc: context.read<TVShowBloc>(),
+      buildWhen: (TVShowState previous, TVShowState current) =>
+          previous.tvOnTheAirTVShowList != current.tvOnTheAirTVShowList,
+      builder: (_, TVShowState state) {
+        if (state.tvOnTheAirTVShowList.isNotEmpty) {
+          return TVShowCardListWidget(
+            title: TVShowText.tvOnTheAir,
+            tvShowList: state.tvOnTheAirTVShowList,
+          );
+        }
+
+        // default loading
+        return const MovieCardListShimmerWidget(title: TVShowText.tvOnTheAir);
+      },
+    );
   }
 
   Widget _buildPopularSection() {
-    return Container();
+    return BlocBuilder<TVShowBloc, TVShowState>(
+      bloc: context.read<TVShowBloc>(),
+      buildWhen: (TVShowState previous, TVShowState current) => previous.popularTVShowList != current.popularTVShowList,
+      builder: (_, TVShowState state) {
+        if (state.popularTVShowList.isNotEmpty) {
+          return TVShowCardListWidget(
+            title: TVShowText.popular,
+            tvShowList: state.popularTVShowList,
+          );
+        }
+
+        // default loading
+        return const MovieCardListShimmerWidget(title: TVShowText.popular);
+      },
+    );
   }
 
   @override
